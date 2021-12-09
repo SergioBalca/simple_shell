abb874fcf09a1415ca1779a82e571ab4ae2f9c11
SergioBalca <3896@holbertonschool.com>
2021-12-09 12:28:53 -0800

    a non_builtin funciont was added and betty was evaluated

---
+++ non_builtin.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * non_builtin- executes a command with its
+ * arguments
+ * @command_arg: the command arguments
+ * @command: the command to execute
+ * @sig: ctr + c signal
+ * Return: 0 on success.
+ */
+
+int non_builtin(char **command_arg, char *command)
+{
+
+	execute(command_arg, command);
+	free(command);
+	command = NULL;
+	free(command_arg);
+	command_arg = NULL;
+	
+	return (0);
+}
